4bad0772f60d74a2628fae6a2341d7e9a7ca6272
Ryan <windsofryannn@yahoo.com>
2021-07-06 18:59:44 -0700

    Completed base UI for TeamPage.js and create MatchPage.js

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,12 +1,22 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import {TeamPage} from "./pages/TeamPage";
 
 function App() {
-  return (
-    <div className="App">
-      <TeamPage />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Router>
+                <Switch>
+                    <Route path="/teams/:teamName/matches/:year">
+                        <MatchPage/>
+                    </Route>
+                    <Route path="/teams/:teamName">
+                        <TeamPage/>
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/frontend/src/pages/MatchPage.js
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react';
+import {useParams} from 'react-router-dom'
+import {MatchDetailCard} from "../components/MatchDetailCard";
+import {MatchSmallCard} from "../components/MatchSmallCard";
+
+export const TeamPage = () => {
+
+    return (
+        <div className="MatchPage">
+            <h1>Match page loaded</h1>
+        </div>
+    );
+}
